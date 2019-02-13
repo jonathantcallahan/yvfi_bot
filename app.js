8907c1cb4a45ee9148167e267781a45bdd6bc041
Jonathan <jtc7537@gmail.com>
2019-02-13 12:01:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const Snoowrap = require('snoowrap')
+const Snoostorm = require('snoostorm')
+
+const r = new Snoowrap({
+    userAgent: 'reddit-bot-example-node',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASSWORD
+})
+
+const client = new Snoostorm(r)
+
+streamOpts = {
+    subreddit: 'testingground4bots',
+    results: 25
+}
+
+const comments = client.CommentStream(streamOpts)
+
+comments.on('comment', comment => {
+    console.log(comment.body)
+    comment.reply('test')
+})
